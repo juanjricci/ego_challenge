be416a7a04821048e3060a46dfb53242b4e38ca8
Juan Ricci <j.ricci@alumno.um.edu.ar>
2024-03-11 10:26:35 -0300

    Created project and models: Modelo, Componente, Ficha

---
+++ test_dev/ego/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+class Modelo(models.Model):
+
+    auto = 'AUTO'
+    pup = 'PICKUP'
+    com = 'COMERCIAL'
+    suv = 'SUV'
+    xover = 'CROSSOVER'
+
+    TIPO_OPS = (
+        (auto, 'Auto'),
+        (pup, 'Pickup'),
+        (com, 'Comercial'),
+        (suv, 'SUV'),
+        (xover, 'Crossover'),
+    )
+    tipo = models.CharField(max_length=200, choices=TIPO_OPS, default=auto)
+    modelo = models.CharField(max_length=200)
+    anio = models.IntegerField()
+    precio = models.DecimalField(max_digits=19, decimal_places=2)
+    thumbnail = models.ImageField(upload_to='car_thumbnails', blank=True)
+
+    def __str__(self):
+        # return str(self.pk)
+        return self.modelo
+
+class Componente(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='components_images')
+
+    def __str__(self):
+        # return str(self.pk)
+        return self.titulo
+
+class Ficha(models.Model):
+    imagen = models.ImageField(upload_to='car_images')
+    descripcion = models.TextField()
+    modelo = models.OneToOneField(Modelo, on_delete=models.CASCADE)
+    componentes = models.ManyToManyField(Componente)
+
+    def __str__(self):
+        return str(self.pk)
