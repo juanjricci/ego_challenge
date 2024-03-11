70c617a235446c4308678db78f88ba11a05388d5
Juan Ricci <j.ricci@alumno.um.edu.ar>
2024-03-11 10:44:06 -0300

    created admin site

--- test_dev/ego/admin.py
+++ test_dev/ego/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Modelo, Componente, Ficha
 
 # Register your models here.
+admin.site.register(Modelo)
+admin.site.register(Componente)
+admin.site.register(Ficha)
