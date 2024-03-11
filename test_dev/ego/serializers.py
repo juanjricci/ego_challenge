c134f2685ab02857b27aefa1e038ad0c67a73e9c
Juan Ricci <j.ricci@alumno.um.edu.ar>
2024-03-11 10:48:47 -0300

    added model serializers

---
+++ test_dev/ego/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Modelo, Ficha, Componente
+
+class ModeloSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Modelo
+        fields=('tipo', 'modelo', 'anio', 'precio', 'thumbnail')
+
+class ComponenteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Componente
+        fields=('titulo', 'descripcion', 'imagen')
+
+class FichaSerializer(serializers.ModelSerializer):
+
+    modelo = ModeloSerializer(read_only=True)
+    componente = ComponenteSerializer(many=True, read_only=True)
+
+    class Meta:
+        model=Ficha
+        fields=('imagen', 'descripcion', 'modelo', 'componente')
